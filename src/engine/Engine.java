80cb899d4116436b103f955532fc4a53c8949348
ghostlypi <parth.iyer@gmail.com>
2021-10-24 15:56:47 -0400

    fixed quarantined spelling mistake

--- src/engine/Engine.java
+++ src/engine/Engine.java
@@ -8,13 +8,13 @@ public class Engine {
 
     public PlayerSet hiders;
     public PlayerSet seekers;
-    public PlayerSet quarentined;
+    public PlayerSet quarantined;
     public GeoFence fence;
 
     public Engine(int capacity){
         hiders = new PlayerSet(capacity);
         seekers = new PlayerSet(capacity);
-        quarentined = new PlayerSet(capacity);
+        quarantined = new PlayerSet(capacity);
     }
 
     public void addPlayer(Player p){
@@ -57,12 +57,12 @@ public class Engine {
     public void inFence(){
         for(UUID uuid : seekers.uuids){
             if(!fence.in(seekers.getPlayer(uuid))){
-                quarentined.addPlayer(seekers.removePlayer(uuid));
+                quarantined.addPlayer(seekers.removePlayer(uuid));
             }
         }
         for(UUID uuid : hiders.uuids){
             if(!fence.in(hiders.getPlayer(uuid))){
-                quarentined.addPlayer(hiders.removePlayer(uuid));
+                quarantined.addPlayer(hiders.removePlayer(uuid));
             }
         }
     }
@@ -70,20 +70,20 @@ public class Engine {
     public void disconnected(){
         for(UUID uuid : seekers.uuids){
             if(!seekers.getPlayer(uuid).status){
-                quarentined.addPlayer(seekers.removePlayer(uuid));
+                quarantined.addPlayer(seekers.removePlayer(uuid));
             }
         }
         for(UUID uuid : hiders.uuids){
             if(!hiders.getPlayer(uuid).status){
-                quarentined.addPlayer(hiders.removePlayer(uuid));
+                quarantined.addPlayer(hiders.removePlayer(uuid));
             }
         }
     }
 
     public void rejoin(){
-        for(UUID uuid : quarentined.uuids){
-            if(fence.in(quarentined.getPlayer(uuid))){
-                Player p = quarentined.removePlayer(uuid);
+        for(UUID uuid : quarantined.uuids){
+            if(fence.in(quarantined.getPlayer(uuid))){
+                Player p = quarantined.removePlayer(uuid);
                 if(p.seeker && p.status){
                     seekers.addPlayer(p);
                 }else if(p.status){

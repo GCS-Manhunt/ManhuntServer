43d1e8a303f72c750c1aebec271f8fabf3e85816
Alan Abraham <abrahamalan2@gmail.com>
2021-11-07 15:49:30 -0500

    added EventMakeSeeker and EventEnterCode

---
+++ src/network/event/EventMakeSeeker.java
@@ -0,0 +1,18 @@
+package network.event;
+
+import io.netty.buffer.ByteBuf;
+
+public class EventMakeSeeker extends PersonalEvent{
+    @Override
+    public void write(ByteBuf b) {
+    }
+
+    @Override
+    public void read(ByteBuf b) {
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
